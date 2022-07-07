ce61b7c06ae2108c671c150d23da0f99f018d842
moikapy <moikapy@devmoi.com>
2022-07-07 16:28:30 -0400

    add twitter to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,18 @@ import Navbar from '../src/components/Navbar';
 export default function Dragon() {
   return (
     <div className='h-100 w-100 d-flex flex-column justify-content-center align-items-center'>
-   
       <h1>Tako Labs</h1>
       <hr />
       <p>Welcome Home</p>
+      <p>
+        Twitter:{' '}
+        <a
+          href='https://twitter.com/takolabsio'
+          target={'_blank'}
+          ref={'noReferral'}>
+          @takolabsio
+        </a>
+      </p>
     </div>
   );
 }
