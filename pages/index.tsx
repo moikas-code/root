f6689091bc05be83323b569e957c3145abd4137c
moikapy <moikapy@devmoi.com>
2022-07-07 16:31:55 -0400

    fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,11 @@ export default function Dragon() {
   return (
     <div className='h-100 w-100 d-flex flex-column justify-content-center align-items-center'>
       <h1>Tako Labs</h1>
+      <h4>Welcome Home</h4>
       <hr />
-      <p>Welcome Home</p>
       <p>
         Twitter:{' '}
-        <a
-          href='https://twitter.com/takolabsio'
-          target={'_blank'}
-          ref={'noReferral'}>
+        <a href='https://twitter.com/takolabsio' target={'_blank'}>
           @takolabsio
         </a>
       </p>
