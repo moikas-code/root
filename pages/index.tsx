5c577284a818cc90ac8d7f346e79d88862d1d1a2
moikapy <moikapy@devmoi.com>
2022-07-08 16:20:59 -0400

    update home text

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Dragon() {
           <h4 className='border-bottom border-dark mb-3'>The Ecosystem</h4>
           <div className='w-100 d-flex flex-row justify-content-around flex-wrap'>
             <Card
-              title={'Create Your Portal'}
+              title={'Deploy A NFT Smart Contract'}
               body={
                 'Deploy A Smart Contract to Permanently Store Your Data On the Blockchain.'
               }
@@ -34,7 +34,7 @@ export default function Dragon() {
               onClick={() => router.push('https://deploy.takolabs.io')}
             />
             <Card
-              title={'Store Your Data'}
+              title={'Create A NFT'}
               body={
                 'Upload your data to the Blockchain as an NFT. This will allow you to share your data with anyone.'
               }
@@ -42,7 +42,7 @@ export default function Dragon() {
               onClick={() => router.push('https://mint.takolabs.io')}
             />
             <Card
-              title={'Trade Your Data'}
+              title={'Trade Your NFT'}
               body={
                 'If you are an Artist, Content Creator, or a Collector, you can trade your NFTS with others.'
               }
@@ -51,17 +51,46 @@ export default function Dragon() {
             />
           </div>
         </div>
-        <div className='s2 p-5 w-100 d-flex flex-column justify-content-center align-items-center'></div>
+        <div className='s2 p-5 w-100 d-flex flex-column justify-content-center align-items-center'>
+          <span>
+            Tako Labs is an Open Source project. You can contribute to the
+            project to help us improve the ecosystem. Our goal is to create
+            tools to help introduce new users to the blockchain while improving
+            the user experience.
+          </span>
+        </div>
       </div>
-      <div className='footer px-3 py-1 d-flex flex-row border border-dark'>
-        <span>
-          Twitter:{' '}
+      <div className='footer px-3 py-1 d-flex flex-row justify-content-center border border-dark'>
+        <span className='mx-2'>
           <TakoLink
             href='https://twitter.com/takolabsio'
             target={'_blank'}
             rel='noreferrer'
-            as={'https://twitter.com/takolabsio'}>
-            @takolabsio
+            as={'https://twitter.com/takolabsio'}
+            className={''}>
+            Twitter
+          </TakoLink>
+        </span>
+        <span className='mx-2'>
+          {' '}
+          <TakoLink
+            href='https://discord.gg/8R6ZMww3KJ'
+            target={'_blank'}
+            rel='noreferrer'
+            className={''}
+            as={'https://discord.gg/8R6ZMww3KJ'}>
+            Discord
+          </TakoLink>
+        </span>
+        <span className='mx-2'>
+          {' '}
+          <TakoLink
+            href='https://github.com/tako-labs'
+            target={'_blank'}
+            rel='noreferrer'
+            className={''}
+            as={'https://github.com/tako-labs'}>
+            Github
           </TakoLink>
         </span>
       </div>
